a8d494d99024b1823485696a3cfbcaf44dfc02ce
stephie-liu <66147533+stephieliu@users.noreply.github.com>
2024-10-09 22:58:59 -0400

    Learning about props
    - Props allow for passing dynamic data through react components
    --> basically act like variables/properties/attributes; can make tasks like creating profiles much easier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import './App.css';
 
-// Functional component
+// Functional components
 // This is specifically an arrow function component
+const Person = (props) => {
+  return(
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
+// Props - allow passing dynamic data through react components
+// passed via attributes (basically properties)
 const App = () => {
-  const name = 'John';
-  
   return (
     <div className="App">
+      {/* All the code from Person just got imported straight into App! */}
+      <Person
+        name={'John'}
+        lastName={'Doe'}
+        age={25}
+      />
+      <Person
+        name='Jane'
+        lastName='Lin'
+        age={2+2}
+      />
+
       <h1>Hello!</h1>
-      {name?(
-        <>
-          <h1>{name}</h1>
-        </>
-      ): (
-        <>
-          <h1>test</h1>
-          <h2>There is no name</h2>
-        </>
-      )
-      }
     </div>
   );
 }
